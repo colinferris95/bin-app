f7ed5b6d2bf51b1f00e545284ab1f9d7518ba7eb
Colin Ferris <cferris@quotient.com>
2023-09-28 11:16:44 -0400

    functions for bin

--- module/BinUtil/src/Controller/BinUtilController.php
+++ module/BinUtil/src/Controller/BinUtilController.php
@@ -12,7 +12,12 @@ class BinUtilController extends AbstractActionController
     private $bin;
     public function indexAction()
     {
-        $bin = new Bin('width');
+        $response;
+        $inputArray = array(0.1, 3.4, 3.5, 3.6, 7.0, 9.0, 6.0, 4.4, 2.5, 3.9, 4.5, 2.8);
+        $bin = new Bin("width",$inputArray);
+        if ($bin->getFilterType == "width"){
+            $response = $bin->widthFilter();
+        }
         var_dump(php_ini_loaded_file(), php_ini_scanned_files());
         echo "hello world!!";
     }
--- module/BinUtil/src/Model/Bin.php
+++ module/BinUtil/src/Model/Bin.php
@@ -4,13 +4,29 @@ namespace BinUtil\Model;
 class Bin{
 
     public $filterType;
+    public $high;
+    public $medium;
+    public $low;
+    public $input;
 
-    function __construct($filterType){
+    //initialize bin with filter type either "width" or "frequency", and then input numbers
+    function __construct($filterType, $input){
         $this->filterType = $filterType;
+        $this->input = $input;
     }
 
     public function getFitlerType(){
         return $this->filterType;
+    }
+
+    //divide high medium and low bins by width intervals
+    public function widthFilter(){
+
+    }
+
+    //divide high medium and low bins by equal amount of numbers
+    public function frequencyFilter(){
+
     }
 
 
