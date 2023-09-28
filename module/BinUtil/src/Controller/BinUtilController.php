26db1d24a02071abb8b895ebe039165bb484df75
Colin Ferris <cferris@quotient.com>
2023-09-28 11:06:08 -0400

    model for the bin

--- module/BinUtil/src/Controller/BinUtilController.php
+++ module/BinUtil/src/Controller/BinUtilController.php
@@ -2,13 +2,17 @@
 
 namespace BinUtil\Controller;
 
+use BinUtil\Model\Bin;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 
+
 class BinUtilController extends AbstractActionController
 {
+    private $bin;
     public function indexAction()
     {
+        $bin = new Bin('width');
         var_dump(php_ini_loaded_file(), php_ini_scanned_files());
         echo "hello world!!";
     }
--- module/BinUtil/src/Module.php
+++ module/BinUtil/src/Module.php
@@ -1,6 +1,9 @@
 <?php
 namespace BinUtil;
 
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\ResultSet\ResultSet;
+use Laminas\Db\TableGateway\TableGateway;
 use Laminas\ModuleManager\Feature\ConfigProviderInterface;
 
 class Module implements ConfigProviderInterface
@@ -8,5 +11,19 @@ class Module implements ConfigProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\Bin::class => function($container) {
+                    return new Model\Bin(
+                        $container->get(Model\Bin::class)
+                    );
+                },
+            ],
+        ];
     }
 }
